5bc7656cead5aed0e85d12ff4b4a050fd51624ec
dbcks203 <dbcks203@naver.com>
2022-11-08 18:06:33 +0900

    multiChatDB v0.1

---
+++ socketdb/src/client/ChatClientMain.java
@@ -0,0 +1,238 @@
+package client;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Scanner;
+
+interface Menu {
+	void displayMenu();
+	
+	enum MenuMode {
+		 USER_MANAGEMENT
+		,CHATTING_MANAGEMENT  
+		,CHAT_ROOM_MANAGEMENT  
+	}
+	
+}
+
+class MenuUserManagement implements Menu {
+	public void displayMenu() {
+		System.out.println();
+		System.out.println("1. 로그인");
+		System.out.println("2. 회원가입");
+		System.out.println("3. 비밀번호검색");
+		System.out.println("4. 회원정보수정");
+		System.out.println("5. 회원탈퇴");
+		System.out.println("6. 전체회원목록");
+		System.out.println("q. 프로그램 종료");
+		System.out.print("메뉴 선택 => ");
+	}
+}
+
+class MenuChattingManagement implements Menu {
+	public void displayMenu() {
+		System.out.println();
+		System.out.println("1. 채팅방 목록");
+		System.out.println("2. 채팅방 생성");
+		System.out.println("3. 채팅방 입장");
+		System.out.println("4. 채팅방 삭제");
+		System.out.println("q. 로그 아웃");
+		System.out.print("메뉴 선택 => ");
+	}
+}
+
+class MenuChatRoomManagement implements Menu {
+	public void displayMenu() {
+		System.out.println();
+		System.out.println("1. 채팅 로그 출력");
+		System.out.println("2. 메세지 입력");
+		System.out.println("3. 파일전송");
+		System.out.println("4. 파일목록 조회");
+		System.out.println("5. 파일 다운");
+		System.out.println("q. 채팅방 나가기");
+		System.out.print("메뉴 선택 => ");
+	}
+}
+
+class MenuFactory {
+	private static Map<Menu.MenuMode, Menu> menuMap = new HashMap<>();
+	static {
+		menuMap.put(Menu.MenuMode.USER_MANAGEMENT, new MenuUserManagement());
+		menuMap.put(Menu.MenuMode.CHATTING_MANAGEMENT, new MenuChattingManagement());
+		menuMap.put(Menu.MenuMode.CHAT_ROOM_MANAGEMENT, new MenuChatRoomManagement());
+	}
+	
+	public static Menu get(Menu.MenuMode menu) {
+		return menuMap.get(menu);
+	}
+}
+
+
+
+public class ChatClientMain {
+	
+	private static Menu menu = MenuFactory.get(Menu.MenuMode.USER_MANAGEMENT);
+	
+	public static void clientUi() {
+		menu.displayMenu();
+	}
+
+	static boolean stop = false;
+	static boolean isMember = false;
+	static boolean isEnter = false;
+//	static MenuMode menuLayer = MenuMode.USER_MANAGEMENT;
+	static Properties prop = new Properties();
+	static Scanner scanner = new Scanner(System.in);
+	static Map<String, Method> actionUserManagement = new HashMap<>(); 
+	
+	static ClientControlMember memberClient = new ClientControlMember(scanner, () -> {
+		menu = MenuFactory.get(Menu.MenuMode.CHATTING_MANAGEMENT);
+	}, () -> {
+		scanner.close();
+		stop = true;
+		System.out.println("프로그램 종료됨");
+	});
+	static ClientControlChat chattingClient = new ClientControlChat();
+	static ClientControlFile fileClient = new ClientControlFile();
+	
+	public static void main(String[] args) throws Exception {
+		
+		prop.load(new FileInputStream(new File("db.properties")));
+		
+		final int count = Integer.parseInt(prop.getProperty("memberManagement.count", "7"));
+		Class cls = memberClient.getClass();
+		
+		for (int i=1;i<=count;i++) {
+			String methodKey = "memberManagement." + i;
+			String methodName = prop.getProperty(methodKey);
+			String [] methodInfo = methodName.split(",");
+			Method method = cls.getMethod(methodInfo[1]);
+			
+			actionUserManagement.put(methodInfo[0], method);
+		}
+		
+		
+		while(!stop) {
+			
+			memberManagement();
+			
+			while(isMember && !isEnter) {
+				clientUi();
+				String menuNum = scanner.nextLine();
+				switch(menuNum) {
+				case "1":
+					chattingClient.chatList();
+
+					break;
+
+				case "2":
+					chattingClient.chatCreate(scanner);
+					break;
+				case "3":
+					isEnter = chattingClient.chatEnter(scanner);
+					if(isEnter) {
+						//menuLayer = MenuMode.CHAT_ROOM_MANAGEMENT;
+						menu = new MenuChatRoomManagement();
+					}
+					break;
+				case "4":
+					chattingClient.removeRoom(scanner);
+					break;
+				case "Q", "q":
+					//menuLayer = MenuMode.USER_MANAGEMENT;
+					menu = MenuFactory.get(Menu.MenuMode.USER_MANAGEMENT);
+					System.out.println("로그아웃");
+					isMember= false;
+					break;
+
+				}
+			}
+
+			
+
+			while(isEnter) {
+				clientUi();
+				String menuNum = scanner.nextLine();
+				switch(menuNum) {
+				case "1"://채팅 로그 출력"
+					fileClient.printChatLog();
+					break;
+				case "2"://메세지 입력"
+					chattingClient.sendMessage(scanner);
+					break;
+				case "3"://파일 전송
+					fileClient.fileTrasfer(scanner);
+					break;
+				case "4"://파일 목록 조회
+					fileClient.fileListOutput();
+					break;
+				case "5"://파일 받기
+					fileClient.fileReceive(scanner);
+					break;
+				case "Q", "q":
+					isEnter= false;
+					//menuLayer = MenuMode.CHATTING_MANAGEMENT;
+					menu = MenuFactory.get(Menu.MenuMode.CHATTING_MANAGEMENT);
+					System.out.println("채팅방에서 나갔습니다.");
+					break;
+
+				}
+			}
+		}
+
+	}
+
+	private static void memberManagement() {
+		
+		while(true) {
+
+			clientUi();
+
+			String menuNum = scanner.nextLine();
+			try {
+				Object ret = actionUserManagement.get(menuNum).invoke(memberClient);
+				if (ret instanceof Boolean) {
+					if (((Boolean)ret).booleanValue()) {
+						return;
+					}
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+//			switch(menuNum) {
+//			case "1":
+//				if (login()) { 
+//					return;
+//				}
+//				break;
+//
+//			case "2":
+//				memberClient.registerMember(); //회원가입
+//				break;
+//			case "3":
+//				memberClient.passwdSearch(); //비밀번호찾기
+//				break;
+//			case "4":
+//				memberClient.updateMember(); //회원정보수정
+//				break;
+//			case "5":
+//				memberClient.memberDelete(); //회원탈퇴
+//				break;
+//			case "6":
+//				memberClient.memberInfo(); //전체회원목록
+//				break;
+//			case "Q", "q":
+//				scanner.close();
+//				stop = true;
+//				System.out.println("프로그램 종료됨");
+//				return;
+//			}
+		}
+	} 
+
+}
---
+++ socketdb/src/client/ClientControlMember.java
@@ -0,0 +1,310 @@
+package client;
+
+import java.util.Scanner;
+import org.json.JSONObject;
+
+
+public class ClientControlMember extends ChatClient {
+
+	private Scanner scanner = null;
+
+	static interface LoginListener {
+		void afterLogin(); 
+	}
+
+	static interface ExitListener {
+		void afterExit(); 
+	}
+
+	LoginListener loginListener = null;
+	ExitListener  exitListener = null;
+
+	public ClientControlMember(Scanner scanner
+			, LoginListener loginListener
+			, ExitListener exitListener) {
+		this.scanner = scanner;
+		this.loginListener = loginListener;
+		this.exitListener = exitListener;
+	}
+
+
+	public boolean login() {
+		try {
+			String uid;
+			String pwd;
+			boolean result = false;
+			System.out.println("\n1. 로그인 작업");
+			System.out.print("아이디 : ");
+			uid = scanner.nextLine();
+			System.out.print("비밀번호 : ");
+			pwd = scanner.nextLine();
+
+			connect();
+
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("memberCommand", "login");
+			jsonObject.put("uid", uid);
+			jsonObject.put("pwd", pwd);
+
+
+			send(jsonObject.toString());
+
+			result = loginResponse();
+
+			disconnect();
+			return result;
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean loginResponse() throws Exception {
+		String json = dis.readUTF();
+		JSONObject root = new JSONObject(json);
+		String statusCode = root.getString("statusCode");
+		String message = root.getString("message");
+
+
+		if (statusCode.equals("0")) {
+			String uid = root.getString("uid");
+			System.out.println("로그인 성공");	
+			System.out.println(uid+"님이 로그인 하셨습니다.");
+			return true;
+		} else {
+			System.out.println(message);
+			return false;
+		}
+	}
+
+	// 회원가입
+	public boolean registerMember() {
+		String uid;
+		String pwd;
+		String name;
+
+		try {
+			System.out.println("[2]회원가입");
+			System.out.print("아이디 : ");
+			uid = scanner.nextLine();
+			System.out.print("비번 : ");
+			pwd = scanner.nextLine();
+			System.out.print("이름 : ");
+			name = scanner.nextLine();
+			connect();
+
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("memberCommand", "registerMember");
+			jsonObject.put("uid", uid);
+			jsonObject.put("pwd", pwd);
+			jsonObject.put("name", name);
+			String json = jsonObject.toString();
+
+			send(json);
+
+			registerMemberResonse();
+			System.out.println(name+"님 환영합니다.");
+
+			disconnect();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+
+	public void registerMemberResonse() throws Exception {
+		String json = dis.readUTF();
+		JSONObject root = new JSONObject(json);
+		String statusCode = root.getString("statusCode");
+		String message = root.getString("message");
+
+		if (statusCode.equals("0")) {
+			System.out.println(message);
+		} else {
+			System.out.println(message);
+		}
+	}
+
+
+
+	public boolean passwdSearch() {
+		try {
+			String uid;
+
+			System.out.println("\n3. 비밀번호 찾기");
+			System.out.print("아이디 : ");
+			uid = scanner.nextLine();
+
+			connect();
+
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("memberCommand", "passwdSearch");
+			jsonObject.put("uid", uid);
+			String json = jsonObject.toString();
+			send(json);
+
+			passwdSearchResponse();
+
+			disconnect();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public void passwdSearchResponse() throws Exception {
+		String json = dis.readUTF();
+		JSONObject root = new JSONObject(json);
+		String statusCode = root.getString("statusCode");
+		String message = root.getString("message");
+
+		if (statusCode.equals("0")) {
+			System.out.println("비밀번호 : " + root.getString("pwd"));
+			System.out.println("정상적으로 실행되었습니다");
+		} else {
+			System.out.println(message);
+		}
+	}
+
+	public boolean updateMember() {
+		String uid;
+		String pwd;
+		String name;
+		try {
+			System.out.println("[4]회원정보수정");
+			System.out.println("변경할 내용을 입력하세요.");
+			System.out.print("아이디 : ");
+			uid = scanner.nextLine();
+			System.out.print("비번 : ");
+			pwd = scanner.nextLine();
+			System.out.print("이름 : ");
+			name = scanner.nextLine();
+
+			connect();
+
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("memberCommand", "updateMember");
+			jsonObject.put("uid", uid);
+			jsonObject.put("pwd", pwd);
+			jsonObject.put("name", name);
+			String json = jsonObject.toString();
+			send(json);
+
+			updateMemberResponse();
+
+			disconnect();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public void updateMemberResponse() throws Exception {
+		String json = dis.readUTF();
+		JSONObject root = new JSONObject(json);
+		String statusCode = root.getString("statusCode");
+		String message = root.getString("message");
+
+		if (statusCode.equals("0")) {
+			System.out.println(message);
+		} else {
+			System.out.println(message);
+		}
+	}
+
+	public boolean memberDelete() {
+		String uid;
+		String pwd;
+
+		try {
+			System.out.println("[5]회원탈퇴");
+			System.out.print("아이디 : ");
+			uid = scanner.nextLine();
+			System.out.print("비번 : ");
+			pwd = scanner.nextLine();
+
+
+			connect();
+
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("memberCommand", "memberDelete");
+			jsonObject.put("uid", uid);
+			jsonObject.put("pwd", pwd);
+
+			String json = jsonObject.toString();
+			send(json);
+
+			updateMemberResponse();
+
+			disconnect();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public void memberDeleteResponse() throws Exception {
+		String json = dis.readUTF();
+		JSONObject root = new JSONObject(json);
+		String statusCode = root.getString("statusCode");
+		String message = root.getString("message");
+
+		if (statusCode.equals("0")) {
+			System.out.println(message);
+		} else {
+			System.out.println(message);
+		}
+	}
+
+	public boolean memberInfo() {
+
+
+		try {
+			System.out.println("[회원목록]");
+			connect();
+
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("memberCommand", "memberInfo");
+			String json = jsonObject.toString();
+
+			send(json);
+
+			memberInfoResonse();
+			disconnect();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+
+	}
+
+	public void memberInfoResonse() throws Exception {
+		String json = dis.readUTF();
+		JSONObject root = new JSONObject(json);
+		String statusCode = root.getString("statusCode");
+		String message = root.getString("message");
+
+
+		if (statusCode.equals("0")) {			
+			System.out.println(message);
+		} else {
+			System.out.println(message);
+		}
+	}
+	public boolean memberExit() {
+		if (exitListener != null) {
+			exitListener.afterExit();
+		}
+		return true;
+	}
+
+
+}
